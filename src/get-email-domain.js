2d2dbb614ffa391f2ecf5890f2935d11d309733f
Lado923 <slava923@gmail.com>
2022-05-03 02:30:34 +0300

    feat: add soputions

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,24 +10,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
- function encodeLine(str) {
-  let newStr = '';
-  let count = 1;
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === str[i + 1]) {
-      count++;
-    }else{
-      if (count > 1) {
-        newStr += count + str[i];
-      } else {
-        newStr += str[i];
-      }
-      count = 1;
-    }
-  }
-  return newStr;
-}
+// function getEmailDomain(/* email */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
 
-module.exports = {
-  getEmailDomain
-};
+// module.exports = {
+//   getEmailDomain
+// };
